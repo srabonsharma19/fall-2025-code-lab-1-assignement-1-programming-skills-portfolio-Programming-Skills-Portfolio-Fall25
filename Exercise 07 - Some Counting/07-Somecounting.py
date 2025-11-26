9bc5f725f7eed74ec7b06650f632bb381ffcb9ff
srabonsharma19 <srabonsharma1904@gmail.com>
2025-11-26 10:12:16 +0400

    Add files via upload

---
+++ Exercise 07 - Some Counting/07-Somecounting.py
@@ -0,0 +1,24 @@
+print("Loop 1:")
+for num in range(51):  
+    print(num, end=" ")
+print("\n")  
+
+print("Loop 2:")
+for num in range(50, -1, -1):
+    print(num, end=" ")
+print("\n")
+
+print("Loop 3:")
+for num in range(30, 51):
+    print(num, end=" ")
+print("\n")
+
+print("Loop 4:")
+for num in range(50, 9, -2):
+    print(num, end=" ")
+print("\n")
+
+print("Loop 5:")
+for num in range(100, 201, 5):
+    print(num, end=" ")
+print("\n")
